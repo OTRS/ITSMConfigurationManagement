bd10185e1c02096b1e1a43c4ba3aad89584ae36d
Udo Bretz <udo.bretz@otrs.com>
2008-08-09 10:35:48 +0000

    Fixed Bug# 3169 - Wildcard search in configitems does not work.

--- Kernel/System/ITSMConfigItem/XML.pm
+++ Kernel/System/ITSMConfigItem/XML.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMConfigItem/XML.pm - sub module of ITSMConfigItem.pm with xml functions
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: XML.pm,v 1.2 2008-08-09 09:48:26 ub Exp $
+# $Id: XML.pm,v 1.3 2008-08-09 10:35:48 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 =head1 NAME
 
@@ -280,10 +280,21 @@ sub _XMLVersionSearch {
 
     # prepare like string
     for my $Row ( @{ $Param{What} } ) {
-        for my $Array ( values %{$Row} ) {
-            for my $Value ( @{$Array} ) {
-                $Self->_PrepareLikeString( \$Value );
+
+        VALUES:
+        for my $Values ( values %{$Row} ) {
+
+            next VALUES if !$Values;
+
+            if ( ref $Values eq 'ARRAY' ) {
+                for my $Value ( @{$Values} ) {
+                    $Self->_PrepareLikeString( \$Value );
+                }
+
+                next VALUES;
             }
+
+            $Self->_PrepareLikeString( \$Values );
         }
     }
 
@@ -560,6 +571,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =head1 VERSION
 
-$Revision: 1.2 $ $Date: 2008-08-09 09:48:26 $
+$Revision: 1.3 $ $Date: 2008-08-09 10:35:48 $
 
 =cut
