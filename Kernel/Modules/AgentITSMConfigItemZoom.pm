81374bfa388938b9c477496409c5c11baa10d56e
Udo Bretz <udo.bretz@otrs.com>
2009-10-06 20:27:52 +0000

    Fixed bug# 4376 - Unusable/Defect Back-Link on CI Linkpage.

--- Kernel/Modules/AgentITSMConfigItemZoom.pm
+++ Kernel/Modules/AgentITSMConfigItemZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMConfigItemZoom.pm - the OTRS::ITSM config item zoom module
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentITSMConfigItemZoom.pm,v 1.3.4.1 2009-09-03 10:04:32 ub Exp $
+# $Id: AgentITSMConfigItemZoom.pm,v 1.3.4.2 2009-10-06 20:27:52 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::ITSMConfigItem;
 use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3.4.1 $) [1];
+$VERSION = qw($Revision: 1.3.4.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -305,6 +305,13 @@ sub Run {
             },
         );
     }
+
+    # store last screen
+    $Self->{SessionObject}->UpdateSessionID(
+        SessionID => $Self->{SessionID},
+        Key       => 'LastScreenView',
+        Value     => $Self->{RequestedURL},
+    );
 
     # start template output
     $Output .= $Self->{LayoutObject}->Output(
