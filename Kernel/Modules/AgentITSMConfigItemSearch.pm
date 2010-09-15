b369df186bbec039f74d11d1f6fc0810eb9152b1
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-09-15 18:01:04 +0000

    Fixed URL for "Change search options" in order to use $LQdata in the dtl link tag

--- Kernel/Modules/AgentITSMConfigItemSearch.pm
+++ Kernel/Modules/AgentITSMConfigItemSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMConfigItemSearch.pm - the OTRS::ITSM config item search module
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentITSMConfigItemSearch.pm,v 1.16 2010-09-15 17:37:30 cr Exp $
+# $Id: AgentITSMConfigItemSearch.pm,v 1.17 2010-09-15 18:01:04 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::SearchProfile;
 use Kernel::System::CSV;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.16 $) [1];
+$VERSION = qw($Revision: 1.17 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -795,7 +795,7 @@ sub Run {
                     TotalHits => scalar( @{$SearchResultList} ),
                     Class     => $ClassList->{$ClassID},
                     ClassID   => $ClassID,
-                    URL       => $URL,
+                    Profile   => $Self->{Profile},
                 },
             );
 
