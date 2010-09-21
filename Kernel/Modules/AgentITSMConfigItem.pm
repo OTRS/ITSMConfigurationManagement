8f46add53e0ccda92430a08c2ec8ea2c2e711b92
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-09-21 22:18:53 +0000

    Added new Class Selector in order solve multiple elements with same id

--- Kernel/Modules/AgentITSMConfigItem.pm
+++ Kernel/Modules/AgentITSMConfigItem.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMConfigItem.pm - the OTRS::ITSM config item module
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentITSMConfigItem.pm,v 1.11 2010-08-25 19:00:24 cg Exp $
+# $Id: AgentITSMConfigItem.pm,v 1.12 2010-09-21 22:18:53 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::ITSMConfigItem;
 use Kernel::System::GeneralCatalog;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.11 $) [1];
+$VERSION = qw($Revision: 1.12 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -144,11 +144,21 @@ sub Run {
         },
     );
 
-    # output ActionAddItem
+    # generate ClassOptionStrg
+    my $SearchAccClassOptionStrg = $Self->{LayoutObject}->BuildSelection(
+        Data         => $ClassList,
+        Name         => 'SearchAccClassID',
+        SelectedID   => $ClassID,
+        Class        => 'W100pc ReloadSelect',
+        PossibleNone => 1,
+        Translation  => 0,
+    );
+
+    # output ActionSearchItem
     $Self->{LayoutObject}->Block(
         Name => 'ActionSearchItem',
         Data => {
-            ClassOptionStrg => $ClassOptionStrg,
+            ClassOptionStrg => $SearchAccClassOptionStrg,
             %Param,
         },
     );
