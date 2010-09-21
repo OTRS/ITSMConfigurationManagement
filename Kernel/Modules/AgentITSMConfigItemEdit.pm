af6e30987cb1a165b396397fa9e2543637d99db8
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-09-21 22:20:13 +0000

    Added ItemID to add fields in order to have a valid "for" label attibute and vaid id for the field

--- Kernel/Modules/AgentITSMConfigItemEdit.pm
+++ Kernel/Modules/AgentITSMConfigItemEdit.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMConfigItemEdit.pm - the OTRS::ITSM config item edit module
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentITSMConfigItemEdit.pm,v 1.15 2010-09-09 22:19:45 mp Exp $
+# $Id: AgentITSMConfigItemEdit.pm,v 1.16 2010-09-21 22:20:13 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::ITSMConfigItem;
 use Kernel::System::GeneralCatalog;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.15 $) [1];
+$VERSION = qw($Revision: 1.16 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -701,6 +701,7 @@ sub _XMLFormOutput {
             $Self->{LayoutObject}->Block(
                 Name => 'XMLRowAddContent',
                 Data => {
+                    ItemID      => $InputKey . 'Add',
                     Name        => $Item->{Name},
                     Description => $Item->{Description} || $Item->{Name},
                     Colspan     => 4 - $Param{Level},
