7417786c45f0d550f30116b708d5330c6dc16ec7
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-12-10 02:54:20 +0000

    Simplified confition

--- Kernel/Modules/AgentITSMConfigItemSearch.pm
+++ Kernel/Modules/AgentITSMConfigItemSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMConfigItemSearch.pm - the OTRS::ITSM config item search module
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentITSMConfigItemSearch.pm,v 1.21 2010-10-04 23:43:37 cr Exp $
+# $Id: AgentITSMConfigItemSearch.pm,v 1.22 2010-12-10 02:54:20 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::SearchProfile;
 use Kernel::System::CSV;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.21 $) [1];
+$VERSION = qw($Revision: 1.22 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -381,7 +381,7 @@ sub Run {
         my $SearchDialog = $Self->{ParamObject}->GetParam( Param => 'SearchDialog' );
 
         # fill up profile name (e.g. with last-search)
-        if ( ( !$Self->{Profile} || !$Self->{SaveProfile} ) && !$Self->{TakeLastSearch} ) {
+        if ( !$Self->{Profile} || !$Self->{SaveProfile} ) {
             $Self->{Profile} = 'last-search';
         }
 
