01f9edf3bd07d4461730808c912734b5ecf4ba09
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-12-17 22:28:54 +0000

    Added dynamcWidth as parameter

--- Kernel/Modules/AgentITSMConfigItemSearch.pm
+++ Kernel/Modules/AgentITSMConfigItemSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMConfigItemSearch.pm - the OTRS::ITSM config item search module
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentITSMConfigItemSearch.pm,v 1.22 2010-12-10 02:54:20 cr Exp $
+# $Id: AgentITSMConfigItemSearch.pm,v 1.23 2010-12-17 22:28:37 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::SearchProfile;
 use Kernel::System::CSV;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.22 $) [1];
+$VERSION = qw($Revision: 1.23 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -362,6 +362,7 @@ sub Run {
                 queryDelay          => $AutoCompleteConfig->{QueryDelay} || 0.1,
                 typeAhead           => $AutoCompleteConfig->{TypeAhead} || 'false',
                 maxResultsDisplayed => $AutoCompleteConfig->{MaxResultsDisplayed} || 20,
+                dynamicWidth        => $AutoCompleteConfig->{DynamicWidth} || 1,
             },
         );
 
