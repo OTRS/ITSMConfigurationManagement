8794ea35f45fe94f76393bc6effcba912820e27b
Michiel Beijen <michiel.beijen@otrs.com>
2012-11-15 07:09:35 +0000

    Added CustomerCompany backend type for ITSM CMDB.

---
+++ Kernel/Output/HTML/ITSMConfigItemLayoutCustomerCompany.pm
@@ -0,0 +1,267 @@
+# --
+# Kernel/Output/HTML/ITSMConfigItemLayoutCustomerCompany.pm - layout backend module
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# --
+# $Id: ITSMConfigItemLayoutCustomerCompany.pm,v 1.1 2012-11-15 07:09:35 mb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::ITSMConfigItemLayoutCustomerCompany;
+
+use strict;
+use warnings;
+
+use Kernel::System::CustomerCompany;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::Output::HTML::ITSMConfigItemLayoutCustomerCompany - layout backend module
+
+=head1 SYNOPSIS
+
+All layout functions of customer company objects
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+    $BackendObject = Kernel::Output::HTML::ITSMConfigItemLayoutCustomerCompany->new(
+        %Param,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (
+        qw(ConfigObject EncodeObject LogObject MainObject ParamObject DBObject LayoutObject)
+        )
+    {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    # create additional objects
+    $Self->{CustomerCompanyObject} = Kernel::System::CustomerCompany->new( %{$Self} );
+
+    return $Self;
+}
+
+=item OutputStringCreate()
+
+create output string
+
+    my $Value = $BackendObject->OutputStringCreate(
+        Value => 11,       # (optional)
+    );
+
+=cut
+
+sub OutputStringCreate {
+    my ( $Self, %Param ) = @_;
+
+    # transform ascii to html
+    $Param{Value} = $Self->{LayoutObject}->Ascii2Html(
+        Text => $Param{Value} || '',
+        HTMLResultMode => 1,
+    );
+
+    return $Param{Value};
+}
+
+=item FormDataGet()
+
+get form data as hash reference
+
+    my $FormDataRef = $BackendObject->FormDataGet(
+        Key => 'Item::1::Node::3',
+        Item => $ItemRef,
+    );
+
+=cut
+
+sub FormDataGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Item)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    my %FormData;
+
+    # get form data
+    $FormData{Value} = $Self->{ParamObject}->GetParam( Param => $Param{Key} );
+
+    # set invalid param
+    if ( $Param{Item}->{Input}->{Required} && !$FormData{Value} ) {
+        $FormData{Invalid} = 1;
+        $Param{Item}->{Form}->{ $Param{Key} }->{Invalid} = 1;
+    }
+
+    return \%FormData;
+}
+
+sub InputCreate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Item)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    my $SelectedID = $Param{Value} || $Param{Item}->{Input}->{ValueDefault} || '';
+
+    my $CSSClass = '';
+    my $Required = $Param{Required};
+    my $Invalid  = $Param{Invalid};
+    my $ItemId   = $Param{ItemId};
+
+    if ($Required) {
+        $CSSClass .= 'Validate_Required';
+    }
+
+    if ($Invalid) {
+        $CSSClass .= ' ServerError';
+    }
+
+    # get class list
+    my %CompanyList = $Self->{CustomerCompanyObject}->CustomerCompanyList();
+
+    # generate string
+    my $String = $Self->{LayoutObject}->BuildSelection(
+        Data         => \%CompanyList,
+        Name         => $Param{Key},
+        ID           => $ItemId,
+        PossibleNone => 1,
+        Translation  => 0,
+        SelectedID   => $SelectedID,
+        Class        => $CSSClass,
+    );
+
+    return $String;
+}
+
+=item SearchFormDataGet()
+
+get search form data
+
+    my $Value = $BackendObject->SearchFormDataGet(
+        Key => 'Item::1::Node::3',
+        Item => $ItemRef,
+    );
+
+=cut
+
+sub SearchFormDataGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{Key} ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'Need Key!',
+        );
+        return;
+    }
+
+    # get form data
+    my $Value;
+    if ( $Param{Value} ) {
+        $Value = $Param{Value};
+    }
+    else {
+        $Value = $Self->{ParamObject}->GetParam( Param => $Param{Key} );
+    }
+    return $Value;
+}
+
+=item SearchInputCreate()
+
+create a search input string
+
+    my $Value = $BackendObject->SearchInputCreate(
+        Key => 'Item::1::Node::3',
+    );
+
+=cut
+
+sub SearchInputCreate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Item)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    my $Values = $Self->SearchFormDataGet(%Param);
+
+    # get company data
+    my %CompanyList = $Self->{CustomerCompanyObject}->CustomerCompanyList();
+
+    # generate string
+    my $String = $Self->{LayoutObject}->BuildSelection(
+        Data        => \%CompanyList,
+        Name        => $Param{Key},
+        Size        => 5,
+        Multiple    => 1,
+        Translation => 0,
+        SelectedID  => $Values,
+    );
+
+    return $String;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2012-11-15 07:09:35 $
+
+=cut
+
---
+++ Kernel/System/ITSMConfigItem/XML/Type/CustomerCompany.pm
@@ -0,0 +1,242 @@
+# --
+# Kernel/System/ITSMConfigItem/XML/Type/CustomerCompany.pm - xml backend module
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# --
+# $Id: CustomerCompany.pm,v 1.1 2012-11-15 07:09:35 mb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::ITSMConfigItem::XML::Type::CustomerCompany;
+
+use strict;
+use warnings;
+
+use Kernel::System::CustomerCompany;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::System::ITSMConfigItem::XML::Type::CustomerCompany - xml backend module
+
+=head1 SYNOPSIS
+
+All xml functions of customer company objects
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+    use Kernel::Config;
+    use Kernel::System::Encode;
+    use Kernel::System::Log;
+    use Kernel::System::DB;
+    use Kernel::System::Main;
+    use Kernel::System::ITSMConfigItem;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $EncodeObject = Kernel::System::Encode->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $LogObject = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+    my $ConfigItemObject = Kernel::System::ITSMConfigItem->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        DBObject     => $DBObject,
+        MainObject   => $MainObject,
+    );
+    $BackendObject = $ConfigItemObject->_LoadXMLTypeBackend(
+        Type => 'CustomerCompany',
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (qw(DBObject ConfigObject EncodeObject LogObject MainObject)) {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+    $Self->{CustomerCompanyObject} = Kernel::System::CustomerCompany->new( %{$Self} );
+
+    return $Self;
+}
+
+=item ValueLookup()
+
+get the xml data of a version
+
+    my $Value = $BackendObject->ValueLookup(
+        Value => 11, # (optional)
+    );
+
+=cut
+
+sub ValueLookup {
+    my ( $Self, %Param ) = @_;
+
+    return '' if !$Param{Value};
+
+    my %CustomerCompany = $Self->{CustomerCompanyObject}->CustomerCompanyGet(
+        CustomerID => $Param{Value},
+    );
+
+    return $CustomerCompany{CustomerCompanyName} || '';
+}
+
+=item StatsAttributeCreate()
+
+create a attribute array for the stats framework
+
+    my $Attribute = $BackendObject->StatsAttributeCreate(
+        Key => 'Key::Subkey',
+        Name => 'Name',
+        Item => $ItemRef,
+    );
+
+=cut
+
+sub StatsAttributeCreate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Name Item)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    # create attribute
+    my $Attribute = [
+        {
+            Name             => $Param{Name},
+            UseAsXvalue      => 0,
+            UseAsValueSeries => 0,
+            UseAsRestriction => 1,
+            Element          => $Param{Key},
+            Block            => 'InputField',
+        },
+    ];
+
+    return $Attribute;
+}
+
+=item ExportSearchValuePrepare()
+
+prepare search value for export
+
+    my $ArrayRef = $BackendObject->ExportSearchValuePrepare(
+        Value => 11, # (optional)
+    );
+
+=cut
+
+sub ExportSearchValuePrepare {
+    my ( $Self, %Param ) = @_;
+
+    return if !defined $Param{Value};
+    return $Param{Value};
+}
+
+=item ExportValuePrepare()
+
+prepare value for export
+
+    my $Value = $BackendObject->ExportValuePrepare(
+        Value => 11, # (optional)
+    );
+
+=cut
+
+sub ExportValuePrepare {
+    my ( $Self, %Param ) = @_;
+
+    return if !defined $Param{Value};
+    return $Param{Value};
+}
+
+=item ImportSearchValuePrepare()
+
+prepare search value for import
+
+    my $ArrayRef = $BackendObject->ImportSearchValuePrepare(
+        Value => 11, # (optional)
+    );
+
+=cut
+
+sub ImportSearchValuePrepare {
+    my ( $Self, %Param ) = @_;
+
+    return if !defined $Param{Value};
+    return $Param{Value};
+}
+
+=item ImportValuePrepare()
+
+prepare value for import
+
+    my $Value = $BackendObject->ImportValuePrepare(
+        Value => 11, # (optional)
+    );
+
+=cut
+
+sub ImportValuePrepare {
+    my ( $Self, %Param ) = @_;
+
+    return if !defined $Param{Value};
+    return $Param{Value};
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2012-11-15 07:09:35 $
+
+=cut
