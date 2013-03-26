654f9d7c6babe4fab11afa7dfb12d8d2023049e4
Udo Bretz <udo.bretz@otrs.com>
2013-03-26 14:14:06 +0000

    Updated file to latest framework version.

--- Kernel/System/Service/PreferencesDB.pm
+++ Kernel/System/Service/PreferencesDB.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/Service/PreferencesDB.pm - some user functions
-# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
 # --
-# $Id: PreferencesDB.pm,v 1.4 2012-11-21 20:37:30 ub Exp $
+# $Id: PreferencesDB.pm,v 1.5 2013-03-26 14:14:06 ub Exp $
 # $OldId: PreferencesDB.pm,v 1.7 2012/11/20 15:56:30 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
@@ -17,8 +17,7 @@ use warnings;
 
 use Kernel::System::CacheInternal;
 
-use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.4 $) [1];
+use vars qw(@ISA);
 
 sub new {
     my ( $Type, %Param ) = @_;
