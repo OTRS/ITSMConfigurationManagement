287da111ed521ab1ede88b241671710392cec93b
Udo Bretz <udo.bretz@otrs.com>
2013-12-05 12:27:50 +0100

    Updated simplified chinese translation. Thanks to Michael Shi!

--- Kernel/Language/zh_CN_ITSMConfigItem.pm
+++ Kernel/Language/zh_CN_ITSMConfigItem.pm
@@ -2,6 +2,8 @@
 # Kernel/Language/zh_CN_ITSMConfigItem.pm - translation file
 # Copyright (C) 2001-2013 OTRS AG, http://otrs.com/
 # --
+# Copyright (C) 2013 Michael Shi <micshi at 163.com>
+# --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
