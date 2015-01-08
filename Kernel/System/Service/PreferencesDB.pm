052093f8c041bfd6c216aaed282368c8ae8e9e90
Udo Bretz <udo.bretz@otrs.com>
2015-01-08 14:24:05 +0100

    Updated files to latest framework version.

--- Kernel/System/Service/PreferencesDB.pm
+++ Kernel/System/Service/PreferencesDB.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Service/PreferencesDB.pm - some user functions
 # Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
 # --
-# $origin: https://github.com/OTRS/otrs/blob/c4828176240c938e4f72b3fe0c6450093b2f032c/Kernel/System/Service/PreferencesDB.pm
+# $origin: https://github.com/OTRS/otrs/blob/114b67598635606fe4fe400aa980c4701e913038/Kernel/System/Service/PreferencesDB.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
