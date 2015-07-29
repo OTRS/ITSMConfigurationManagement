00aa5f61543e18eb59f83d625c6866e396bd0c4a
Udo Bretz <udo.bretz@otrs.com>
2015-07-29 16:17:20 +0200

    Code cleanup.

--- var/httpd/htdocs/js/ITSM.Agent.ConfigItem.Search.js
+++ var/httpd/htdocs/js/ITSM.Agent.ConfigItem.Search.js
@@ -1,6 +1,6 @@
 // --
 // ITSM.Agent.ConfigItem.Search.js - provides the special module functions for the global search
-// Copyright (C) 2001-2013 OTRS AG, http://otrs.org/\n";
+// Copyright (C) 2001-2013 OTRS AG, http://otrs.com
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
