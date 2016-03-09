b79413b75541aabd2cc271c3d9d6755880d01109
Udo Bretz <udo.bretz@otrs.com>
2016-03-09 13:03:44 +0100

    Improved ConfigItemCreate operation to be able to specify the CI number as parameter.

--- Kernel/GenericInterface/Operation/ConfigItem/ConfigItemCreate.pm
+++ Kernel/GenericInterface/Operation/ConfigItem/ConfigItemCreate.pm
@@ -129,6 +129,7 @@ perform ConfigItemCreate Operation. This will return the created config item num
             Password  => 'some password',                   # if UserLogin is sent then
                                                             #   Password is required
             ConfigItem => {
+                Number       => '111',                      # optional
                 Class        => 'Config Item Class',
                 Name         => 'The Name',
                 DeplState    => 'deployment state',
@@ -624,6 +625,7 @@ sub _ConfigItemCreate {
 
     # create new config item
     my $ConfigItemID = $ConfigItemObject->ConfigItemAdd(
+        Number  => $ConfigItem->{Number},
         ClassID => $Self->{ReverseClassList}->{ $ConfigItem->{Class} },
         UserID  => $Param{UserID},
     );
@@ -653,7 +655,7 @@ sub _ConfigItemCreate {
             my $Result = $Self->CreateAttachment(
                 Attachment   => $Attachment,
                 ConfigItemID => $ConfigItemID,
-                UserID       => $Param{UserID}
+                UserID       => $Param{UserID},
             );
 
             if ( !$Result->{Success} ) {
