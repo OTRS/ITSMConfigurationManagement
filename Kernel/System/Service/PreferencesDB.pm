30c28468cc808e90fc97f8c8b90209562a886a74
Manuel Hecht <manuel.hecht@otrs.com>
2017-01-02 11:54:44 +0100

    Updated copyright.

--- Kernel/System/Service/PreferencesDB.pm
+++ Kernel/System/Service/PreferencesDB.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
 # --
 # $origin: otrs - d29ce846afdfb52eebed219c0671bfcc0d75e5c9 - Kernel/System/Service/PreferencesDB.pm
 # --
