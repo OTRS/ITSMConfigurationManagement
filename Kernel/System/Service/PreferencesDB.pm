a8e3742829b678d5326c2675702e409b97880971
Manuel Hecht <manuel.hecht@otrs.com>
2017-01-17 15:37:53 +0100

    Updated origins.

--- Kernel/System/Service/PreferencesDB.pm
+++ Kernel/System/Service/PreferencesDB.pm
@@ -1,7 +1,7 @@
 # --
 # Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
 # --
-# $origin: otrs - ec0d79d1961308387ad27398528763b5db1142ca - Kernel/System/Service/PreferencesDB.pm
+# $origin: otrs - dcb4bca6b4f9d5de2ece16c2022c994effb9e589 - Kernel/System/Service/PreferencesDB.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
