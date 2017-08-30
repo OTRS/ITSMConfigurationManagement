f13ba02270a1c061edc1227eb2d04ecc43fabfc9
Andre Bras <andre.bras@otrs.com>
2017-08-30 11:26:12 +0100

    OTRS 6 porting: 3c script patch.

--- Kernel/System/Service/PreferencesDB.pm
+++ Kernel/System/Service/PreferencesDB.pm
@@ -1,7 +1,7 @@
 # --
 # Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
 # --
-# $origin: otrs - be4010f3365da552dcfd079c36ad31cc90e06c32 - Kernel/System/Service/PreferencesDB.pm
+# $origin: otrs - 32e2ee9e20388db9ae10a1c7414b274272b7c936 - Kernel/System/Service/PreferencesDB.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
