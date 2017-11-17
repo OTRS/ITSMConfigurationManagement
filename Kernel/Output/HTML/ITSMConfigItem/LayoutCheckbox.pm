62d4cc8d3fed1797e9b7a6d06ffef9985a6bdd73
Mstislav Martynyuk <martynyuk.mstislav@gmail.com>
2017-11-17 13:21:48 +0300

    New CMDB logical attribute: checkbox element

---
+++ Kernel/Output/HTML/ITSMConfigItem/LayoutCheckbox.pm
@@ -0,0 +1,335 @@
+# --
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::ITSMConfigItem::LayoutCheckbox;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::System::Log',
+    'Kernel::Output::HTML::Layout',
+    'Kernel::Config',
+    'Kernel::System::Web::Request'
+);
+
+=head1 NAME
+
+Kernel::Output::HTML::ITSMConfigItem::LayoutCheckbox - layout backend module
+
+=head1 SYNOPSIS
+
+All layout functions of text objects
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+    $BackendObject = Kernel::Output::HTML::ITSMConfigItem::LayoutCheckbox->new(
+        %Param,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+=item OutputStringCreate()
+
+create output string
+
+    my $Value = $BackendObject->OutputStringCreate(
+        Value => 11,       # (optional)
+        Item => $ItemRef,
+    );
+
+=cut
+
+sub OutputStringCreate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{Item} ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'error',
+            Message  => 'Need Item!',
+        );
+        return;
+    }
+
+    if ( !defined $Param{Value} ) {
+        $Param{Value} = '';
+    }
+
+    # get layout object
+    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    $Param{Value} = $Param{Value} = '1' ? 'Yes' : 'No' ;
+
+    # translate
+    if ( $Param{Item}->{Input}->{Translation} ) {
+        $Param{Value} = $LayoutObject->{LanguageObject}->Translate( $Param{Value} );
+    }
+
+    my $LinkFeature = 1;
+
+    # do not transform links in print view
+    if ( $Param{Print} ) {
+        $LinkFeature = 0;
+    }
+
+    # transform ascii to html
+    $Param{Value} = $LayoutObject->Ascii2Html(
+        Text           => $Param{Value},
+        HTMLResultMode => 1,
+        LinkFeature    => $LinkFeature,
+    );
+
+    return $Param{Value};
+}
+
+=item FormDataGet()
+
+get form data as hash reference
+
+    my $FormDataRef = $BackendObject->FormDataGet(
+        Key => 'Item::1::Node::3',
+        Item => $ItemRef,
+    );
+
+=cut
+
+sub FormDataGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Item)) {
+        if ( !$Param{$Argument} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    my %FormData;
+
+    # get form data
+    $FormData{Value} = $Kernel::OM->Get('Kernel::System::Web::Request')->GetParam( Param => $Param{Key} );
+
+    # set invalid param
+    if ( $Param{Item}->{Input}->{Required} && !$FormData{Value} ) {
+        $FormData{Invalid} = 1;
+        $Param{Item}->{Form}->{ $Param{Key} }->{Invalid} = 1;
+    }
+
+    # value was entered in the form, a regex is defined and the value does not match the regex
+    if (
+        $FormData{Value}
+        && $Param{Item}->{Input}->{RegEx}
+        && $FormData{Value} !~ m{ $Param{Item}->{Input}->{RegEx} }xms
+        )
+    {
+
+        $FormData{Invalid}                                         = 1;
+        $Param{Item}->{Form}->{ $Param{Key} }->{Invalid}           = 1;
+        $Param{Item}->{Form}->{ $Param{Key} }->{RegExErrorMessage} = $Param{Item}->{Input}->{RegExErrorMessage};
+    }
+
+    return \%FormData;
+}
+
+=item InputCreate()
+
+create a input string
+
+    my $Value = $BackendObject->InputCreate(
+        Key => 'Item::1::Node::3',
+        Value => 11,                # (optional)
+        Item => $ItemRef,
+    );
+
+=cut
+
+sub InputCreate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Item)) {
+        if ( !$Param{$Argument} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    my $Value = $Param{Value};
+    if ( !defined $Param{Value} ) {
+        $Value = $Param{Item}->{Input}->{ValueDefault} || 0;
+    }
+
+    my $Class    = '';
+    my $Size     = '';
+    my $Required = $Param{Required};
+    my $Invalid  = $Param{Invalid};
+    my $ItemId   = $Param{ItemId};
+    my $Title    = $Param{Item}->{Input}->{Title} || '';
+    my $TitleEx  = $Param{Item}->{Input}->{ExtendedTitle} || 0;
+
+    if ($Required) {
+        $Class .= ' Validate_Required';
+    }
+
+    if ($Invalid) {
+        $Class .= ' ServerError';
+    }
+    if ($Title ne "") {
+        $Title = "title=\"$Title\"";
+    }
+    $Class .= ' ' . $Size;
+    my $String = "<input type=\"checkbox\" name=\"$Param{Key}\" class=\"$Class\" ${Title} ";
+
+    if ($ItemId) {
+        $String .= "id=\"$ItemId\" ";
+    }
+
+    if ($Value) {
+
+        # get layout object
+        my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+
+        # translate
+        if ( $Param{Item}->{Input}->{Translation} ) {
+            $Value = $LayoutObject->{LanguageObject}->Translate($Value);
+        }
+
+        # transform ascii to html
+        $Value = $LayoutObject->Ascii2Html(
+            Text           => $Value,
+            HTMLResultMode => 1,
+        );
+    }
+
+
+    if($Param{Item}->{Input}->{Checked} || $Value){
+	$String .= "checked ";
+	$Value = 1 unless($Value);
+    }
+    
+    $String .= "value=\"$Value\" ";
+
+    # add maximum length
+    if ( $Param{Item}->{Input}->{MaxLength} ) {
+        $String .= "maxlength=\"$Param{Item}->{Input}->{MaxLength}\" ";
+    }
+    
+    if($Param{Item}->{Input}->{Disabled}){
+	$String .= "readonly ";
+    }
+
+    $String .= ' onclick="this.value=this.value^1"/>';
+    $String .= (($TitleEx == 1 && $Title ne "") ? '<span class="TooltipEx-button" onclick="TooltipEx(event, \'' . $ItemId . '\')"></span>' : '') . ' ';
+
+    return $String;
+}
+
+=item SearchFormDataGet()
+
+get search form data
+
+    my $Value = $BackendObject->SearchFormDataGet(
+        Key => 'Item::1::Node::3',
+        Item => $ItemRef,
+    );
+
+=cut
+
+sub SearchFormDataGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{Key} ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'error',
+            Message  => 'Need Key!',
+        );
+        return;
+    }
+
+    # get form data
+    my $Value;
+    if ( $Param{Value} ) {
+        $Value = $Param{Value};
+    }
+    else {
+        $Value = $Kernel::OM->Get('Kernel::System::Web::Request')->GetParam( Param => $Param{Key} );
+    }
+    return $Value;
+}
+
+=item SearchInputCreate()
+
+create a search input string
+
+    my $Value = $BackendObject->SearchInputCreate(
+        Key => 'Item::1::Node::3',
+        Item => $ItemRef,
+    );
+
+=cut
+
+sub SearchInputCreate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Item)) {
+        if ( !$Param{$Argument} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    my $Value = $Self->SearchFormDataGet(%Param);
+    if ( !defined $Value ) {
+        $Value = '';
+    }
+
+    my $String = qq{<input type="checkbox" name="$Param{Key}" value="$Value" class="W50pc" >};
+
+    return $String;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
