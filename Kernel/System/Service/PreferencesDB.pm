faf8b3b8df0dbfe70321526373000152a0fc1413
Manuel Hecht <manuel.hecht@otrs.com>
2018-08-23 09:00:19 +0200

    Updated license information.

--- Kernel/System/Service/PreferencesDB.pm
+++ Kernel/System/Service/PreferencesDB.pm
@@ -1,11 +1,11 @@
 # --
-# Copyright (C) 2001-2018 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
 # --
 # $origin: otrs - a4512636fc11eff48b5864b208e064bb1cce29f3 - Kernel/System/Service/PreferencesDB.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
 # --
 
 package Kernel::System::Service::PreferencesDB;
